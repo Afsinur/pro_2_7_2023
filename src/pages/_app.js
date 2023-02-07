b4e1b1d04a7dcb10705a88447b6a31312493bb28
Afsinur <afsin.sayem1@gmail.com>
2023-02-08 00:24:01 +0600

    added01

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
 import "@/styles/topSection.css";
+import "@/styles/partOne.css";
+import "@/styles/partTwo.css";
+import "@/styles/partThree.css";
+import "@/styles/partFour.css";
+import "@/styles/partFive.css";
 import "@/styles/footerSection.css";
 import "@/styles/justAbovFooter.css";
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,11 @@ import { Inter } from "@next/font/google";
 import top_search from "../img/top-search.png";
 import arrow_left from "../img/arrow-left.png";
 import arrow_right from "../img/arrow-right.png";
+import white_arrow_right from "../img/white-arrow-right.png";
 import brand_logo from "../img/brand-logo.png";
+import part_four_right_img from "../img/part-four-right-img.png";
+import part_two_left_img from "../img/part-two-left-img.png";
+import part_one_right_img from "../img/part-one-right-img.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +22,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <TopSection></TopSection>
+      <PartOne></PartOne>
+      <PartTwo></PartTwo>
+      <PartThree></PartThree>
+      <PartFour></PartFour>
+      <PartFive></PartFive>
       <JustAbovFooter></JustAbovFooter>
       <FooterSection></FooterSection>
     </>
@@ -69,6 +78,157 @@ function TopSection() {
           Learn More
           <Image src={arrow_right} alt="arrow" />
         </button>
+      </div>
+    </div>
+  );
+}
+
+function PartOne() {
+  return (
+    <div className="part-one">
+      <div>
+        <p>01</p>
+
+        <div className="left">
+          <div>
+            <h1>
+              Search inventory in natural language and build client insights
+            </h1>
+
+            <p>
+              Query your own gallery inventory like you would with a
+              google-search.
+              <br />
+              <br />
+              Discover insights regarding demand, price, aesthetic similarity,
+              and related works and artists to help you find the best fit for
+              your clients preferences
+            </p>
+
+            <button>
+              <span>Try Our Demo</span>
+
+              <Image src={white_arrow_right} alt="arrow" />
+            </button>
+          </div>
+
+          <div>
+            <Image src={part_one_right_img} alt="part-one-img" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function PartTwo() {
+  return (
+    <div className="part-two">
+      <div>
+        <p>02</p>
+
+        <div className="left">
+          <div>
+            <h1>
+              Automate your outbound with collector context and personalization
+            </h1>
+
+            <p>
+              Gain new collectors and build relationships while our AI helps
+              define existing collector tastes to recommend specific artworks
+              <br />
+              <br />
+              Auto-send emails for follow ups, exhibitions & events, or new
+              inventory
+              <br />
+              <br />
+              Use analytics to understand the efficacy of your outbounds and
+              revenue generated
+            </p>
+
+            <button>
+              <span>Get Early Access</span>
+
+              <Image src={white_arrow_right} alt="arrow" />
+            </button>
+          </div>
+
+          <div>
+            <Image src={part_two_left_img} alt="part-two-img" />
+            <p>
+              *Automating sending an email to collectors about a new piece in
+              today.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function PartThree() {
+  return (
+    <div className="part-three">
+      <div>
+        <p>03</p>
+
+        <div>
+          <div>
+            Galleries were created to nurture artists and provide the
+            relationships and platform for them to become great
+          </div>
+          <div>
+            We are automating tedious business operations so galleries can get
+            back to focusing on building their artists
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function PartFour() {
+  return (
+    <div className="part-four">
+      <div>
+        <p>04</p>
+
+        <div className="left">
+          <div>
+            <h1>Understand the art market better than ever before</h1>
+
+            <p className="line-height">
+              The most comprehensive art database to ever exist Say goodbye to
+              clunky Artnet with UX from the 90s
+            </p>
+
+            <p>
+              Here’s Augier to help galleries, students, and museums find and
+              learn about art with culture and context
+            </p>
+
+            <button>
+              <span>Get Early Access</span>
+
+              <Image src={white_arrow_right} alt="arrow" />
+            </button>
+          </div>
+
+          <div>
+            <Image src={part_four_right_img} alt="part-four-img" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function PartFive() {
+  return (
+    <div className="part-five">
+      <div>
+        <div>05</div>
+        <div>Trusted By</div>
       </div>
     </div>
   );
